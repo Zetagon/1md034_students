1743d45d4f4957c56c35e5301e64e0a2989feac8
Leo Okawa Ericson <github@relevant-information.com>
2020-02-04 10:26:35 +0100

    Task 4-6

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,7 +1,10 @@
-
-let vm = new Vue({
-    el: "#var",
-    data: {
-        hej: "hej"
-    }
+const vm = new Vue({
+  el: '#myID',
+  data: {
+      oreoP: oreo.nameKCal(),
+      megaP: mega.nameKCal(),
+      ufoP: ufo.nameKCal(),
+      billysP: billys.nameKCal(),
+      doritosP: doritos.nameKCal()
+  }
 })
